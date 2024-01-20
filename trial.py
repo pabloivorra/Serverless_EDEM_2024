d0d5d58842e5889abdccf98d60a71746ba48a66c
pabloivorra <145840713+pabloivorra@users.noreply.github.com>
2024-01-20 13:58:53 +0100

    Cambios

--- trial.py
+++ trial.py
@@ -3,9 +3,8 @@ import json
 from apache_beam.io import WriteToBigQuery  # Import the missing module
 
 def decode_message(msg):
-    output = msg.decoded('utf-8')
+    output = msg.decode('utf-8')
     return json.loads(output)
-
 
 # PubSub (PipelineOptions es una clase que permite configurar el pipeline)
 
@@ -14,10 +13,10 @@ from apache_beam.options.pipeline_options import PipelineOptions
 with beam.Pipeline(options=PipelineOptions(streaming=True)) as p:
     (
         p
-        | 'Read from PubSub' >> beam.io.ReadFromPubSub(topic='projects/phonic-obelisk-411712/topics/edem-topic')
+        | 'Read from PubSub' >> beam.io.ReadFromPubSub(subscription='projects/phonic-obelisk-411712/subscriptions/edem-topic-sub')
         | 'Decode' >> beam.Map(decode_message)
         | 'Write to BigQuery' >> WriteToBigQuery(  # Correct the typo in the schema parameter
-            table='phonic-obelisk-411712:edem_dataset.edem_table',
+            table='phonic-obelisk-411712:dataset.edem_tabla',
             schema="nombre:STRING",  # Correct the typo in the schema parameter
             create_disposition=beam.io.BigQueryDisposition.CREATE_NEVER,
             write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND
